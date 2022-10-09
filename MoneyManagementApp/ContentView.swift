3034519baec74c86d28d7a33576da5f0c28af2a1
Michael <michael.castlewellan@outlook.com>
2022-10-09 19:44:42 +0100

    Update ContentView.swift

--- MoneyManagementApp/ContentView.swift
+++ MoneyManagementApp/ContentView.swift
@@ -8,9 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var tax: Int = 0
+    @State var parts: Int = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                Toggle(isOn: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Is On@*/.constant(true)/*@END_MENU_TOKEN@*/) {
+                    Text("MOT")
+                }
+                Toggle(isOn: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Is On@*/.constant(true)/*@END_MENU_TOKEN@*/) {
+                    Text("MOT Re-Test")
+                }
+                VStack {
+                Text("Annual Tax Cost")
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                TextField("Tax", value: $tax, formatter: NumberFormatter())
+                }
+                Button("Save") {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                }
+            }
+            .navigationTitle("Annual Car Costs")
+        }
     }
 }
 
