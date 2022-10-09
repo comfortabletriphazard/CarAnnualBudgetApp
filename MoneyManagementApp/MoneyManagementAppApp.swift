d33f9a084f8f1714fa91c6b538b2d58a08999520
Michael <michael.castlewellan@outlook.com>
2022-10-09 17:01:39 +0100

    Initial Commit

---
+++ MoneyManagementApp/MoneyManagementAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyManagementAppApp.swift
+//  MoneyManagementApp
+//
+//  Created by Michael McAlarney on 09/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct MoneyManagementAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
