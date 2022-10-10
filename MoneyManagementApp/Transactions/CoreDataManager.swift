afc38647be44d97c660ff741a75f56bc30e5a2df
Michael <michael.castlewellan@outlook.com>
2022-10-10 20:29:11 +0100

    Starting to intertwine CoreData

---
+++ MoneyManagementApp/Transactions/CoreDataManager.swift
@@ -0,0 +1,19 @@
+//
+//  CoreDataManager.swift
+//  MoneyManagementApp
+//
+//  Created by Michael McAlarney on 10/10/2022.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataManager {
+    
+    Int persistentContainer: NSPersistentContainer
+    
+    init() {
+        persistentContainer = NSPersistentStoreDescription
+    }
+    
+}
